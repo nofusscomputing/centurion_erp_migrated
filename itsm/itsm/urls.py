c0212178111f000c20b0b60426d3542dd704e8ce
Jon <jonathon.lockwood@networkedweb.com>
2024-05-11 12:14:51 +0930

    feat(development): added the debug django app
    !1

--- itsm/itsm/urls.py
+++ itsm/itsm/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
@@ -27,3 +28,10 @@ urlpatterns = [
     path("account/", include("django.contrib.auth.urls")),
     path("organization/", include("access.urls")),
 ]
+
+if settings.DEBUG:
+
+    urlpatterns += [
+
+        path("__debug__/", include("debug_toolbar.urls"), name='_debug'),
+    ]
