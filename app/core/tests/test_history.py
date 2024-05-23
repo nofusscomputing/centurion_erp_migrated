ae4ef9d14d4b56ea44dd85bdc34cdd1966d854f6
Jon <jonathon.lockwood@networkedweb.com>
2024-05-23 13:10:21 +0930

    test(core): placeholder tests for history component
    !9 #5

---
+++ app/core/tests/test_history.py
@@ -0,0 +1,174 @@
+
+from django.test import TestCase, Client
+
+import pytest
+import unittest
+import requests
+
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_device_create():
+    """ History row must be added to history table on create """
+    pass
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_device_update():
+    """ History row must be added to history table on updatej """
+    pass
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_device_delete():
+    """ History row must be added to history table on delete """
+    pass
+
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_device_operating_system_create():
+    """ History row must be added to history table on create 
+    
+    Must also have populated parent_item_pk and parent_item_class columns
+    """
+    pass
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_device_operating_system_update():
+    """ History row must be added to history table on update
+    
+    Must also have populated parent_item_pk and parent_item_class columns
+    """
+    pass
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_device_operating_system_delete():
+    """ History row must be added to history table on delete
+    
+    Must also have populated parent_item_pk and parent_item_class columns
+    """
+    pass
+
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_device_software_create():
+    """ History row must be added to history table on create
+
+    Must also have populated parent_item_pk and parent_item_class columns
+    """
+    pass
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_device_software_update():
+    """ History row must be added to history table on update
+    
+    Must also have populated parent_item_pk and parent_item_class columns
+    """
+    pass
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_device_software_delete():
+    """ History row must be added to history table on delete
+    
+    Must also have populated parent_item_pk and parent_item_class columns
+    """
+    pass
+
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_operating_system_create():
+    """ History row must be added to history table on create """
+    pass
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_operating_system_update():
+    """ History row must be added to history table on updatej """
+    pass
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_operating_system_delete():
+    """ History row must be added to history table on delete """
+    pass
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_operating_system_version_create():
+    """ History row must be added to history table on create
+    
+    Must also have populated parent_item_pk and parent_item_class columns
+    """
+    pass
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_operating_system_version_update():
+    """ History row must be added to history table on update
+    
+    Must also have populated parent_item_pk and parent_item_class columns
+    """
+    pass
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_operating_system_version_delete():
+    """ History row must be added to history table on delete
+    
+    Must also have populated parent_item_pk and parent_item_class columns
+    """
+    pass
+
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_software_create():
+    """ History row must be added to history table on create """
+    pass
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_software_update():
+    """ History row must be added to history table on updatej """
+    pass
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_software_delete():
+    """ History row must be added to history table on delete """
+    pass
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_software_version_create():
+    """ History row must be added to history table on create
+    
+    Must also have populated parent_item_pk and parent_item_class columns
+    """
+    pass
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_software_version_update():
+    """ History row must be added to history table on update
+    
+    Must also have populated parent_item_pk and parent_item_class columns
+    """
+    pass
+
+
+@pytest.mark.skip(reason="to be written")
+def test_history_software_version_delete():
+    """ History row must be added to history table on delete
+    
+    Must also have populated parent_item_pk and parent_item_class columns
+    """
+    pass
+
