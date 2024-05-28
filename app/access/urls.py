98885a32e71463403f1bb9c535cb6cab39d09733
Jon <jonathon.lockwood@networkedweb.com>
2024-05-29 05:33:10 +0930

    fix(access): use established view naming
    !13

--- app/access/urls.py
+++ app/access/urls.py
@@ -6,8 +6,8 @@ from .views import team, organization, user
 app_name = "Access"
 urlpatterns = [
     path("", organization.IndexView.as_view(), name="Organizations"),
-    path("<int:pk>/", organization.View.as_view(), name="_organization"),
-    path("<int:pk>/edit", organization.Change.as_view(), name="_organization_change"),
+    path("<int:pk>/", organization.View.as_view(), name="_organization_view"),
+    # path("<int:pk>/edit", organization.Change.as_view(), name="_organization_change"),
     path("<int:organization_id>/team/<int:pk>/", team.View.as_view(), name="_team"),
     path("<int:pk>/team/add", team.Add.as_view(), name="_team_add"),
     path("<int:organization_id>/team/<int:pk>/edit", team.Change.as_view(), name="_team_change"),
