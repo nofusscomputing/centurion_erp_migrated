8d786d4dea2ed3b290a83f90c863bbe46e53cefd
Jon <jonathon.lockwood@networkedweb.com>
2024-05-29 05:11:40 +0930

    feat(history): always create an entry even if user=none
    a none user denotes an admin operation, i.e. cli action
    
    !13

--- app/core/mixin/history_save.py
+++ app/core/mixin/history_save.py
@@ -143,18 +143,20 @@ class SaveHistory(models.Model):
 
             current_user = get_request().user
 
-        if current_user:
+            if current_user.is_anonymous:
+                current_user = None
 
-            if before != after and after != '{}' and not current_user.is_anonymous:
-                entry = History.objects.create(
-                    before = before_json,
-                    after = after,
-                    user = current_user,
-                    action = action,
-                    item_pk = self.pk,
-                    item_class = self._meta.model_name,
-                    item_parent_pk = item_parent_pk,
-                    item_parent_class = item_parent_class,
-                )
 
-                entry.save()
+        if before != after and after != '{}':
+            entry = History.objects.create(
+                before = before_json,
+                after = after,
+                user = current_user,
+                action = action,
+                item_pk = self.pk,
+                item_class = self._meta.model_name,
+                item_parent_pk = item_parent_pk,
+                item_parent_class = item_parent_class,
+            )
+
+            entry.save()
