89a5e0f4cc1336e042f242dfeef9a88c37b1d9f4
Jon <jonathon.lockwood@networkedweb.com>
2024-06-02 19:46:23 +0930

    feat(api): filter devices to users organizations
    !17 #45

--- app/api/views/itam/device.py
+++ app/api/views/itam/device.py
@@ -1,15 +1,18 @@
-# from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
 
 from rest_framework import generics, viewsets
 
-from itam.models.device import Device
+from access.mixin import OrganizationMixin
 
 from api.serializers.itam.device import DeviceSerializer
 from api.views.mixin import OrganizationPermissionAPI
 
+from itam.models.device import Device
 
-class DeviceViewSet(viewsets.ModelViewSet):
+
+
+class DeviceViewSet(OrganizationMixin, viewsets.ModelViewSet):
 
     permission_classes = [
         OrganizationPermissionAPI
@@ -18,6 +21,18 @@ class DeviceViewSet(viewsets.ModelViewSet):
     queryset = Device.objects.all()
 
     serializer_class = DeviceSerializer
+
+
+    def get_queryset(self):
+
+        if self.request.user.is_superuser:
+
+            return self.queryset.filter().order_by('name')
+
+        else:
+
+            return self.queryset.filter(Q(organization__in=self.user_organizations()) | Q(is_global = True)).order_by('name')
+
 
     def get_view_name(self):
         return "Device"
