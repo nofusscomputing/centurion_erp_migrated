4151e0afdc6cbc9a253f41441ab0074fe947db01
Jon <jonathon.lockwood@networkedweb.com>
2024-06-02 19:48:14 +0930

    feat(api): filter software to users organizations
    !17 closes #45

--- app/api/views/itam/software.py
+++ app/api/views/itam/software.py
@@ -1,11 +1,12 @@
-# from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
-from rest_framework import generics, viewsets
 
-from itam.models.software import Software
+from rest_framework import generics, viewsets
 
 from api.serializers.itam.software import SoftwareSerializer
 from api.views.mixin import OrganizationPermissionAPI
+
+from itam.models.software import Software
 
 
 
@@ -26,7 +27,14 @@ class SoftwareViewSet(viewsets.ModelViewSet):
 
 
     def get_queryset(self):
-        return Software.objects.all()
+
+        if self.request.user.is_superuser:
+
+            return self.queryset.filter().order_by('name')
+
+        else:
+
+            return self.queryset.filter(Q(organization__in=self.user_organizations()) | Q(is_global = True)).order_by('name')
 
 
     def get_view_name(self):
