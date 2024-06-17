e9c0b67b72ba53141d35aefcaa16f1b9581a9269
Jon <jonathon.lockwood@networkedweb.com>
2024-06-18 04:16:51 +0930

    feat(project_management): add interim project model
    !30

---
+++ app/project_management/models/project_common.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+from access.fields import *
+from access.models import TenancyObject
+
+
+class ProjectCommonFields(TenancyObject, models.Model):
+
+    class Meta:
+        abstract = True
+
+    id = models.AutoField(
+        primary_key=True,
+        unique=True,
+        blank=False
+    )
+
+    created = AutoCreatedField()
+
+    modified = AutoLastModifiedField()
+
+
+
+class ProjectCommonFieldsName(ProjectCommonFields):
+
+    class Meta:
+        abstract = True
+
+    name = models.CharField(
+        blank = False,
+        max_length = 50,
+        unique = True,
+    )
+
+    slug = AutoSlugField()
