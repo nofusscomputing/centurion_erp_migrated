87d782cc53ec1d319bd3a0d50fca3ddb9ebf0d3a
Jon <jonathon.lockwood@networkedweb.com>
2024-06-18 04:51:53 +0930

    feat(project_management): save project history
    !30 #14

--- app/core/views/history.py
+++ app/core/views/history.py
@@ -41,9 +41,11 @@ class View(OrganizationPermission, generic.View):
 
         from config_management.models.groups import ConfigGroups
 
+        from project_management.models.projects import Project
+
         if not hasattr(self, 'model'):
 
-            match self.kwargs['model_name']:
+            match str(self.kwargs['model_name']).lower():
 
                 case 'configgroups':
 
@@ -84,6 +86,10 @@ class View(OrganizationPermission, generic.View):
                 case 'team':
 
                     self.model = Team
+
+                case 'project':
+
+                    self.model = Project
 
                 case _:
                     raise Exception('Unable to determine history items model')
