f7db557be97085b81e3d3ac212b0706c62147655
Jon <jonathon.lockwood@networkedweb.com>
2024-06-18 06:21:02 +0930

    feat(project_management): Project task view "view"
    !30 #14

--- app/project_management/urls.py
+++ app/project_management/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views.project import ProjectIndex, ProjectAdd, ProjectDelete, ProjectChange, ProjectView
-from .views.project_task import ProjectTaskAdd, ProjectTaskDelete, ProjectTaskView
+from .views.project_task import ProjectTaskAdd, ProjectTaskChange, ProjectTaskDelete, ProjectTaskView
 
 app_name = "Project Management"
 urlpatterns = [
@@ -13,8 +13,9 @@ urlpatterns = [
     path("project/<int:pk>/delete", ProjectDelete.as_view(), name="_project_delete"),
 
     path("project/<int:pk>/task/add", ProjectTaskAdd.as_view(), name="_project_task_add"),
-    path("project/<int:project_id>/task/<int:pk>", ProjectTaskView.as_view(), name="_project_task_change"),
+    path("project/<int:project_id>/task/<int:pk>/edit", ProjectTaskChange.as_view(), name="_project_task_change"),
     path("project/<int:project_id>/task/<int:pk>/delete", ProjectTaskDelete.as_view(), name="_project_task_delete"),
+    path("project/<int:project_id>/task/<int:pk>", ProjectTaskView.as_view(), name="_project_task_view"),
 
 
 ]
