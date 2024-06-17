de78a30a5d789d1153feadd404e10cb2e20d5a75
Jon <jonathon.lockwood@networkedweb.com>
2024-06-18 04:26:39 +0930

    feat(project_management): add project view page
    !30 #14

---
+++ app/project_management/forms/project.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.db.models import Q
+
+from app import settings
+from project_management.models.projects import Project
+
+class ProjectForm(forms.ModelForm):
+
+    prefix = 'project'
+
+    class Meta:
+        fields = '__all__'
+
+
+        model = Project
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['planned_start_date'].widget = forms.widgets.DateTimeInput(attrs={'type': 'datetime-local', 'format': "%Y-%m-%dT%H:%M"})
+        self.fields['planned_start_date'].input_formats = settings.DATETIME_FORMAT
+        self.fields['planned_start_date'].format="%Y-%m-%dT%H:%M"
+
+        self.fields['planned_finish_date'].widget = forms.widgets.DateTimeInput(attrs={'type': 'datetime-local'})
+        self.fields['planned_finish_date'].input_formats = settings.DATETIME_FORMAT
+        self.fields['planned_finish_date'].format="%Y-%m-%dT%H:%M"
+
+        self.fields['real_start_date'].widget = forms.widgets.DateTimeInput(attrs={'type': 'datetime-local'})
+        self.fields['real_start_date'].input_formats = settings.DATETIME_FORMAT
+        self.fields['real_start_date'].format="%Y-%m-%dT%H:%M"
+
+        self.fields['real_finish_date'].widget = forms.widgets.DateTimeInput(attrs={'type': 'datetime-local'})
+        self.fields['real_finish_date'].input_formats = settings.DATETIME_FORMAT
+        self.fields['real_finish_date'].format="%Y-%m-%dT%H:%M"
+
