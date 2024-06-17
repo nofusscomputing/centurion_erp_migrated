58466fa490129ede32cec804b006529700310e0b
Jon <jonathon.lockwood@networkedweb.com>
2024-06-18 04:28:46 +0930

    feat(project_management): add project edit page
    !30 #14

--- app/project_management/urls.py
+++ app/project_management/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views.project import ProjectIndex, ProjectAdd, ProjectView
+from .views.project import ProjectIndex, ProjectAdd, ProjectChange, ProjectView
 
 
 app_name = "Project Management"
@@ -9,5 +9,6 @@ urlpatterns = [
 
     path("project/add", ProjectAdd.as_view(), name="_project_add"),
     path("project/<int:pk>", ProjectView.as_view(), name="_project_view"),
+    path("project/<int:pk>/edit", ProjectChange.as_view(), name="_project_change"),
     
 ]
