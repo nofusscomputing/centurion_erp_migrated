58051f297f1ef6dc3dc015f0e4e5e5ccc46d68c2
Jon <jonathon.lockwood@networkedweb.com>
2024-06-19 11:51:03 +0930

    test(access): TenancyObject field tests
    !33 #15

--- app/access/tests/tenancy_object/test_tenancy_object.py
+++ app/access/tests/tenancy_object/test_tenancy_object.py
@@ -3,22 +3,43 @@ import unittest
 
 from django.test import TestCase
 
+from access.models import TenancyObject
+
+from unittest.mock import patch
+
 
 
 class TenancyObject(TestCase):
 
-    # @classmethod
-    # def setUpTestData(self):
-    #     """ Setup Test """
-
-    #     pass
+    item = TenancyObject
 
 
 
-    @pytest.mark.skip(reason="to be written")
-    def test_function_save_attributes(self):
-        """ Ensure save Attributes function match django default
+    def test_has_attribute_organization(self):
+        """ Field organization exists """
+        
+        assert hasattr(self.item, 'organization')
 
-        the save method is overridden. the function attributes must match default django method
-        """
-        pass
+
+    def test_has_attribute_is_global(self):
+        """ Field organization exists """
+        
+        assert hasattr(self.item, 'is_global')
+
+
+    def test_has_attribute_model_notes(self):
+        """ Field organization exists """
+        
+        assert hasattr(self.item, 'model_notes')
+
+
+    def test_has_attribute_get_organization(self):
+        """ Function 'get_organization' Exists """
+        
+        assert hasattr(self.item, 'get_organization')
+
+
+    def test_is_function_get_organization(self):
+        """ Attribute 'get_organization' is a function """
+        
+        assert callable(self.item.get_organization)
