b801c9a49e70ca5640c65bafdd05c29daed40798
Jon <jonathon.lockwood@networkedweb.com>
2024-07-01 00:27:28 +0930

    feat(itam): Update UUID from inventory if present and UUID not set
    !37 fixes #66

--- app/api/views/itam/inventory.py
+++ app/api/views/itam/inventory.py
@@ -122,6 +122,13 @@ this setting populated, no device will be created and the endpoint will return H
                 status = Http.Status.CREATED
 
 
+            if not device.uuid and data.details.uuid:
+
+                device.uuid = data.details.uuid
+
+                device.save()
+
+
             if OperatingSystem.objects.filter( slug=data.operating_system.name ).exists():
 
                 operating_system = OperatingSystem.objects.get( slug=data.operating_system.name )
