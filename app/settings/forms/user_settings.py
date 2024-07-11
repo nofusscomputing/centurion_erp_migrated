9e336d368d51381701b358287853f9ceab61b49f
Jon <jonathon.lockwood@networkedweb.com>
2024-07-11 15:53:50 +0930

    fix(settings): restrict user visible organizations to ones they are part of
    this includes if they are the org manager
    
    !35 fixes #99

--- app/settings/forms/user_settings.py
+++ app/settings/forms/user_settings.py
@@ -1,4 +1,9 @@
 from django import forms
+from django.db.models import Q
+
+from django.contrib.auth.models import User
+
+from access.models import Organization, TeamUsers
 
 from settings.models.user_settings import UserSettings
 
@@ -14,3 +19,23 @@ class UserSettingsForm(forms.ModelForm):
         ]
 
         model = UserSettings
+
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        user_organizations = []
+
+        for team_user in TeamUsers.objects.filter(user=kwargs['instance'].user):
+
+            if team_user.team.organization.name not in user_organizations:
+
+                user_organizations += [ team_user.team.organization.name ]
+
+
+        self.fields['default_organization'].queryset = Organization.objects.filter(
+            Q(name__in=user_organizations)
+            |
+            Q(manager=kwargs['instance'].user)
+        )
