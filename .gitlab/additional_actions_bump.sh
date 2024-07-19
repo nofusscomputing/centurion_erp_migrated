94576cc7338ddd55de7ef3fa3428c377b1b210a0
Jon <jonathon.lockwood@networkedweb.com>
2024-07-19 18:21:43 +0930

    ci: fix additional command as part of release
    !42

--- .gitlab/additional_actions_bump.sh
+++ .gitlab/additional_actions_bump.sh
@@ -2,6 +2,6 @@
 
 # Create Version label wtihn repo
 curl \
-    --data "name=v${$RELEASE_TAG}&color=#eee600&description=Version%20that%20is%20affected" \
+    --data "name=v${RELEASE_TAG}&color=#eee600&description=Version%20that%20is%20affected" \
     --header "PRIVATE-TOKEN: $GIT_COMMIT_TOKEN" \
     "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/labels"
