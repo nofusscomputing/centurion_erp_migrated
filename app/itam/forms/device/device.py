5fff51953f1d098a11a7a3e7cfa1730518cbb4db
Jon <jonathon.lockwood@networkedweb.com>
2024-08-15 22:07:20 +0930

    refactor(itam): device now use details template
    #227 #240

--- app/itam/forms/device/device.py
+++ app/itam/forms/device/device.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.db.models import Q
+from django.urls import reverse
 
 from app import settings
 
@@ -27,11 +28,71 @@ class DeviceForm(CommonModelForm):
         ]
 
 
+
+class DetailForm(DeviceForm):
+
+    tabs: dict = {
+        "details": {
+            "name": "Details",
+            "slug": "details",
+            "sections": [
+                {
+                    "layout": "double",
+                    "left": [
+                        'name',
+                        'device_model',
+                        'serial_number',
+                        'uuid',
+                        'device_type',
+                        'organization',
+                        'c_created',
+                        'c_modified',
+                        'lastinventory',
+                    ],
+                    "right": [
+                        'model_notes',
+                    ]
+                }
+            ]
+        },
+        "software": {
+            "name": "Software",
+            "slug": "software",
+            "sections": []
+        },
+        "notes": {
+            "name": "Notes",
+            "slug": "notes",
+            "sections": []
+        },
+        "config_management": {
+            "name": "Config Management",
+            "slug": "config_management",
+            "sections": []
+        }
+    }
+
+
     def __init__(self, *args, **kwargs):
+
         super().__init__(*args, **kwargs)
 
-        if hasattr(kwargs['instance'], 'inventorydate'):
-            self.fields['lastinventory'] = forms.DateTimeField(
+
+        self.fields['c_created'] = forms.DateTimeField(
+            label = 'Created',
+            input_formats=settings.DATETIME_FORMAT,
+            disabled = True,
+            initial = self.instance.created,
+        )
+
+        self.fields['c_modified'] = forms.DateTimeField(
+            label = 'Modified',
+            input_formats=settings.DATETIME_FORMAT,
+            disabled = True,
+            initial = self.instance.modified,
+        )
+
+        self.fields['lastinventory'] = forms.DateTimeField(
                 label="Last Inventory Date",
                 input_formats=settings.DATETIME_FORMAT,
                 initial=kwargs['instance'].inventorydate,
@@ -39,5 +100,11 @@ class DeviceForm(CommonModelForm):
                 required=False,
             )
 
-        # for key in self.fields.keys():
-        #     self.fields[key].widget.attrs['disabled'] = True
+        self.tabs['details'].update({
+            "edit_url": reverse('ITAM:_device_change', args=(self.instance.pk,))
+        })
+
+        # self.model_name_plural = self.instance._meta.verbose_name_plural
+
+        self.url_index_view = reverse('ITAM:Devices')
+
