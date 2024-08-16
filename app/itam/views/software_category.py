7c1e1e232e9ed1a8e0febb882967ac06798356f4
Jon <jonathon.lockwood@networkedweb.com>
2024-08-16 14:28:16 +0930

    refactor(itam): Software Categories now uses details template
    #242 closes #236

--- app/itam/views/software_category.py
+++ app/itam/views/software_category.py
@@ -4,13 +4,13 @@ from django.utils.decorators import method_decorator
 
 from core.views.common import AddView, ChangeView, DeleteView
 
-from itam.forms.software_category import SoftwareCategoryForm
+from itam.forms.software_category import DetailForm, SoftwareCategoryForm
 from itam.models.software import Software, SoftwareCategory
 
 from settings.models.user_settings import UserSettings
 
 
-class View(ChangeView):
+class Change(ChangeView):
 
     context_object_name = "software"
 
@@ -19,7 +19,6 @@ class View(ChangeView):
     model = SoftwareCategory
 
     permission_required = [
-        'itam.view_softwarecategory',
         'itam.change_softwarecategory',
     ]
 
@@ -45,6 +44,38 @@ class View(ChangeView):
     def post(self, request, *args, **kwargs):
 
         return super().post(request, *args, **kwargs)
+
+
+
+class View(ChangeView):
+
+    context_object_name = "software"
+
+    form_class = DetailForm
+
+    model = SoftwareCategory
+
+    permission_required = [
+        'itam.view_softwarecategory',
+    ]
+
+    template_name = 'itam/software_categories.html.j2'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['model_delete_url'] = reverse('Settings:_software_category_delete', args=(self.kwargs['pk'],))
+
+        context['content_title'] = self.object.name
+
+        return context
+
+
+    def get_success_url(self, **kwargs):
+
+        return reverse('Settings:_software_category_view', args=(self.kwargs['pk'],))
+
 
 
 
