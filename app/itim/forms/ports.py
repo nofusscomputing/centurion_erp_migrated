eb4df77614c5fef566b6ee13ab5f950c6d44a676
Jon <jonathon.lockwood@networkedweb.com>
2024-08-16 16:39:07 +0930

    refactor(itim): Service Port now uses details template
    #242 closes #238

--- app/itim/forms/ports.py
+++ app/itim/forms/ports.py
@@ -1,8 +1,8 @@
 
-# from django import forms
-# from django.forms import ValidationError
+from django import forms
+from django.urls import reverse
 
-# from app import settings
+from app import settings
 
 from itim.models.services import Port
 
@@ -22,3 +22,67 @@ class PortForm(CommonModelForm):
         model = Port
 
     prefix = 'port'
+
+
+
+class DetailForm(PortForm):
+
+    tabs: dict = {
+        "details": {
+            "name": "Details",
+            "slug": "details",
+            "sections": [
+                {
+                    "layout": "double",
+                    "left": [
+                        'number',
+                        'description',
+                        'protocol',
+                        'organization',
+                        'c_created',
+                        'c_modified',
+                        'lastinventory',
+                    ],
+                    "right": [
+                        'model_notes',
+                    ]
+                }
+            ]
+        },
+        "services": {
+            "name": "Services",
+            "slug": "services",
+            "sections": []
+        },
+        "notes": {
+            "name": "Notes",
+            "slug": "notes",
+            "sections": []
+        },
+    }
+
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+
+        self.fields['c_created'] = forms.DateTimeField(
+            label = 'Created',
+            input_formats=settings.DATETIME_FORMAT,
+            disabled = True,
+            initial = self.instance.created,
+        )
+
+        self.fields['c_modified'] = forms.DateTimeField(
+            label = 'Modified',
+            input_formats=settings.DATETIME_FORMAT,
+            disabled = True,
+            initial = self.instance.modified,
+        )
+
+        self.tabs['details'].update({
+            "edit_url": reverse('Settings:_port_change', args=(self.instance.pk,))
+        })
+
+        self.url_index_view = reverse('Settings:_ports')
