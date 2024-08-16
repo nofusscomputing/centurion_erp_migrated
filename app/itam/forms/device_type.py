15e3ba1b82001780b8b0a3dc8867a7b8003b60ac
Jon <jonathon.lockwood@networkedweb.com>
2024-08-16 17:10:48 +0930

    refactor(itam): Device Type now uses details template
    #242 closes #234

--- app/itam/forms/device_type.py
+++ app/itam/forms/device_type.py
@@ -1,4 +1,7 @@
-from django.db.models import Q
+from django import forms
+from django.urls import reverse
+
+from app import settings
 
 from core.forms.common import CommonModelForm
 
@@ -26,3 +29,61 @@ class DeviceTypeForm(
        ]
 
        model = DeviceType
+
+
+
+class DetailForm(DeviceTypeForm):
+
+    tabs: dict = {
+        "details": {
+            "name": "Details",
+            "slug": "details",
+            "sections": [
+                {
+                    "layout": "double",
+                    "left": [
+                       'name',
+                        'slug',
+                        'organization',
+                        'is_global',
+                        'c_created',
+                        'c_modified',
+                    ],
+                    "right": [
+                        'model_notes',
+                    ]
+                }
+            ]
+        },
+        # "notes": {
+        #     "name": "Notes",
+        #     "slug": "notes",
+        #     "sections": []
+        # }
+    }
+
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+
+        self.fields['c_created'] = forms.DateTimeField(
+            label = 'Created',
+            input_formats=settings.DATETIME_FORMAT,
+            disabled = True,
+            initial = self.instance.created,
+        )
+
+        self.fields['c_modified'] = forms.DateTimeField(
+            label = 'Modified',
+            input_formats=settings.DATETIME_FORMAT,
+            disabled = True,
+            initial = self.instance.modified,
+        )
+
+        self.tabs['details'].update({
+            "edit_url": reverse('Settings:_device_type_change', args=(self.instance.pk,))
+        })
+
+        self.url_index_view = reverse('Settings:_device_types')
