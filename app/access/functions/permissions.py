345d33c682fa5808d32846e5d77bc71507e465c8
Jon <jonathon.lockwood@networkedweb.com>
2024-08-17 15:16:34 +0930

    feat(development): Add function to filter permissions to those used by centurion
    ref: #244 #164

---
+++ app/access/functions/permissions.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.models import Permission
+
+def permission_queryset():
+    """Filter Permissions to those used within the application
+
+    Returns:
+        list: Filtered queryset that only contains the used permissions
+    """
+
+    apps = [
+        'access',
+        'assistance',
+        'config_management',
+        'core',
+        'django_celery_results',
+        'itam',
+        'itim',
+        'settings',
+    ]
+
+    exclude_models = [
+        'appsettings',
+        'chordcounter',
+        'groupresult',
+        'organization'
+        'settings',
+        'usersettings',
+    ]
+
+    exclude_permissions = [
+        'add_organization',
+        'add_taskresult',
+        'change_organization',
+        'change_taskresult',
+        'delete_organization',
+        'delete_taskresult',
+    ]
+
+    return Permission.objects.filter(
+            content_type__app_label__in=apps,
+        ).exclude(
+            content_type__model__in=exclude_models
+        ).exclude(
+            codename__in = exclude_permissions
+        )
