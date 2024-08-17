61a4bf2d0933cfbb3ba83ebfc89550dafe738ebb
Jon <jonathon.lockwood@networkedweb.com>
2024-08-17 17:50:08 +0930

    feat(itim): Ability to add and configure cluster
    ref: #244 #71

--- app/itim/urls.py
+++ app/itim/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 
-from itim.views import services
+from itim.views import clusters, services
 
 app_name = "ITIM"
 urlpatterns = [
@@ -11,5 +11,11 @@ urlpatterns = [
     path("service/<int:pk>/edit", services.Change.as_view(), name="_service_change"),
     path("service/<int:pk>/delete", services.Delete.as_view(), name="_service_delete"),
     path("service/<int:pk>", services.View.as_view(), name="_service_view"),
+
+    path("clusters", clusters.Index.as_view(), name="Clusters"),
+    path("clusters/add", clusters.Add.as_view(), name="_cluster_add"),
+    path("clusters/<int:pk>/edit", clusters.Change.as_view(), name="_cluster_change"),
+    path("clusters/<int:pk>/delete", clusters.Delete.as_view(), name="_cluster_delete"),
+    path("clusters/<int:pk>", clusters.View.as_view(), name="_cluster_view"),
 
 ]
