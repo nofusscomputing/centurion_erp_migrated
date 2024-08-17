a5e8b974fb8f4cdd4a87019031b5a100e285f41b
Jon <jonathon.lockwood@networkedweb.com>
2024-08-17 15:20:17 +0930

    feat(api): Endpoint to fetch user permissions
    ref: #244 closes #164

--- app/api/views/index.py
+++ app/api/views/index.py
@@ -1,15 +1,20 @@
-# from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.utils.safestring import mark_safe
 
 from rest_framework import generics, permissions, routers, viewsets
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
+
+
 class Index(viewsets.ViewSet):
 
-    # permission_required = 'access.view_organization'
+    permission_classes = [
+        IsAuthenticated,
+    ]
+
 
     def get_view_name(self):
         return "API Index"
@@ -29,6 +34,7 @@ class Index(viewsets.ViewSet):
             "devices": reverse("API:device-list", request=request),
             "config_groups": reverse("API:_api_config_groups", request=request),
             "organizations": reverse("API:_api_orgs", request=request),
+            "settings": reverse('API:_settings', request=request),
             "software": reverse("API:software-list", request=request),
         }
     )
