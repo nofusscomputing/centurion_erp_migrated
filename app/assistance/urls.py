7b3a00786205ab98fd08862c486a54549660b456
Jon <jonathon.lockwood@networkedweb.com>
2024-08-25 17:48:17 +0930

    feat(assistance): Add Request ticket to navigation
    ref: #250 #252 #96

--- app/assistance/urls.py
+++ app/assistance/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from assistance.views import knowledge_base
 
+from core.views import ticket, ticket_comment
+
 app_name = "Assistance"
 
 urlpatterns = [
@@ -11,5 +13,14 @@ urlpatterns = [
     path("information/<int:pk>/edit", knowledge_base.Change.as_view(), name="_knowledge_base_change"),
     path("information/<int:pk>/delete", knowledge_base.Delete.as_view(), name="_knowledge_base_delete"),
     path("information/<int:pk>", knowledge_base.View.as_view(), name="_knowledge_base_view"),
+
+    path('ticket/request', ticket.Index.as_view(), kwargs={'ticket_type': 'request'}, name="Requests"),
+    path('ticket/<str:ticket_type>/add', ticket.Add.as_view(), name="_ticket_request_add"),
+    path('ticket/<str:ticket_type>/<int:pk>/edit', ticket.Change.as_view(), name="_ticket_request_change"),
+    path('ticket/<str:ticket_type>/<int:pk>', ticket.View.as_view(), name="_ticket_request_view"),
+
+    path('ticket/<str:ticket_type>/<int:ticket_id>/comment/add', ticket_comment.Add.as_view(), name="_ticket_comment_request_add"),
+    path('ticket/<str:ticket_type>/<int:ticket_id>/comment/<int:pk>/edit', ticket_comment.Change.as_view(), name="_ticket_comment_request_change"),
+    path('ticket/<str:ticket_type>/<int:ticket_id>/comment/<int:parent_id>/add', ticket_comment.Add.as_view(), name="_ticket_comment_request_reply_add"),
 
 ]
