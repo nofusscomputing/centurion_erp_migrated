0c3e38c543994ead485b4a88811e7a2f799a65b6
Jon <jonathon.lockwood@networkedweb.com>
2024-09-10 10:58:54 +0930

    test(core): Ticket comment Views
    ref: #250 #96 #93 #95 #90 #264 #267

---
+++ app/core/tests/unit/ticket_comment/test_ticket_comment_views.py
@@ -0,0 +1,34 @@
+import pytest
+import unittest
+import requests
+
+from django.test import TestCase
+
+from app.tests.abstract.models import PrimaryModel, ModelAdd, ModelChange, ModelDelete
+
+
+
+# class TicketCommentViews(
+#     TestCase,
+#     PrimaryModel
+# ):
+class TicketCommentViews(
+    TestCase,
+    ModelAdd,
+    ModelChange,
+):
+
+    add_module = 'core.views.ticket'
+    add_view = 'Add'
+
+    change_module = add_module
+    change_view = 'Change'
+
+    delete_module = add_module
+    delete_view = 'Delete'
+
+    # display_module = add_module
+    # display_view = 'View'
+
+    # index_module = add_module
+    # index_view = 'Index'
