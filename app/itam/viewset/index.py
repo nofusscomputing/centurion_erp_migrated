4e894b72c8b3fc25c158f20079a2934f8e7f1d2b
Jon <jonathon.lockwood@networkedweb.com>
2024-10-12 23:34:43 +0930

    feat(itam): Add v2 endpoint ITAM
    ref:  #345 #346

---
+++ app/itam/viewset/index.py
@@ -0,0 +1,30 @@
+from drf_spectacular.utils import extend_schema
+
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+from api.viewsets.common import CommonViewSet
+
+
+
+@extend_schema(exclude = True)
+class Index(CommonViewSet):
+
+    allowed_methods: list = [
+        'GET',
+        'HEAD',
+        'OPTIONS'
+    ]
+
+    view_description = "Information Technology Asset Management (ITAM)"
+
+    view_name = "ITAM"
+
+
+    def list(self, request, pk=None):
+
+        return Response(
+            {
+                "device": reverse('API:_api_v2_device-list', request=request)
+            }
+        )
