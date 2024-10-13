598e24c77541797392b60aa25c82fd18eb5b6680
Jon <jonathon.lockwood@networkedweb.com>
2024-10-13 15:21:26 +0930

    feat(access): Add attribute table_fields to Team model
    ref:  #248 #345 #346

--- app/access/models.py
+++ app/access/models.py
@@ -308,6 +308,12 @@ class Team(Group, TenancyObject):
         },
     ]
 
+    table_fields: list = [
+        'team_name',
+        'modified',
+        'created',
+    ]
+
     @property
     def parent_object(self):
         """ Fetch the parent object """
