a05cf021c13b375c120831bf7b78981b5da69a98
Jon <jonathon.lockwood@networkedweb.com>
2024-10-14 01:17:06 +0930

    test(access): Team custom tests to ensure that during model field creation, attribute verbose_name is defined and not empty
    as team extends group, filtering of group fields is required so they are not checked when testing
    
    ref:  #248 #345 #346

--- app/access/tests/unit/team/test_team.py
+++ app/access/tests/unit/team/test_team.py
@@ -68,3 +68,73 @@ class TeamModel(
     @pytest.mark.skip(reason="uses Django group manager")
     def test_model_class_tenancy_manager_function_get_queryset_called(self):
         pass
+
+
+    def test_model_fields_parameter_not_empty_help_text(self):
+        """Test Field called with Parameter
+
+        This is a custom test of a test derived of the samae name. It's required
+        as the team model extends the Group model.
+
+        During field creation, paramater `help_text` must not be `None` or empty ('')
+        """
+
+        group_mode_fields_to_ignore: list = [
+            'id',
+            'name',
+            'group_ptr_id'
+        ]
+
+        fields_have_test_value: bool = True
+
+        for field in self.model._meta.fields:
+
+            if field.attname in group_mode_fields_to_ignore:
+
+                continue
+
+            print(f'Checking field {field.attname} is not empty')
+
+            if (
+                field.help_text is None
+                or field.help_text == ''
+            ):
+
+                print(f'    Failure on field {field.attname}')
+
+                fields_have_test_value = False
+
+
+        assert fields_have_test_value
+
+    def test_model_fields_parameter_type_verbose_name(self):
+        """Test Field called with Parameter
+
+        This is a custom test of a test derived of the samae name. It's required
+        as the team model extends the Group model.
+
+        During field creation, paramater `verbose_name` must be of type str
+        """
+
+        group_mode_fields_to_ignore: list = [
+            'name',
+        ]
+
+        fields_have_test_value: bool = True
+
+        for field in self.model._meta.fields:
+
+            if field.attname in group_mode_fields_to_ignore:
+
+                continue
+
+            print(f'Checking field {field.attname} is of type str')
+
+            if not type(field.verbose_name) is str:
+
+                print(f'    Failure on field {field.attname}')
+
+                fields_have_test_value = False
+
+
+        assert fields_have_test_value
