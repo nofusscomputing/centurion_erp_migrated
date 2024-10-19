319eaadbc252ead7c5bfb8c15545e51eaf260712
Jon <jonathon.lockwood@networkedweb.com>
2024-10-19 12:21:00 +0930

    test(core): Notes Serializer Validation checks
    ref: #15 #49 #248 #354

---
+++ app/core/tests/unit/test_notes/test_notes_serializer.py
@@ -0,0 +1,66 @@
+import pytest
+
+from django.test import TestCase
+
+from rest_framework.exceptions import ValidationError
+
+from access.models import Organization
+
+from core.serializers.notes import Notes, NoteModelSerializer
+
+from itam.models.device import Device
+
+
+
+class NotesValidationAPI(
+    TestCase,
+):
+
+    # model = ConfigGroups
+
+    @classmethod
+    def setUpTestData(self):
+        """Setup Test
+
+        1. Create an org
+        2. Create an item
+        """
+
+        organization = Organization.objects.create(name='test_org')
+
+        self.organization = organization
+
+        self.device = Device.objects.create(
+            organization = self.organization,
+            name = 'notes-test'
+        )
+
+        # self.item_no_parent = self.model.objects.create(
+        #     organization=organization,
+        #     name = 'random title',
+        #     config = { 'config_key': 'a value' }
+        # )
+
+        # self.item_has_parent = self.model.objects.create(
+        #     organization=organization,
+        #     name = 'random title two',
+        #     parent = self.item_no_parent
+        # )
+
+
+
+    def test_serializer_validation_no_note(self):
+        """Serializer Validation Check
+
+        Ensure that if creating and no name is provided a validation error occurs
+        """
+
+        with pytest.raises(ValidationError) as err:
+
+            serializer = NoteModelSerializer(data={
+                "organization": self.organization.id,
+            })
+
+            serializer.is_valid(raise_exception = True)
+
+        assert err.value.get_codes()['note'][0] == 'required'
