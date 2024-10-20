fad5ac51686331c739193ff470212b91719cbd23
Jon <jonathon.lockwood@networkedweb.com>
2024-10-20 17:30:41 +0930

    test(itam): Device Model Serializer Validation checks
    ref: #15 #248 #353

---
+++ app/itam/tests/unit/device_model/test_device_model_serializer.py
@@ -0,0 +1,46 @@
+import pytest
+
+from django.test import TestCase
+
+from rest_framework.exceptions import ValidationError
+
+from access.models import Organization
+
+from itam.serializers.device_model import DeviceModel, DeviceModelModelSerializer
+
+
+
+class DeviceModelValidationAPI(
+    TestCase,
+):
+
+    model = DeviceModel
+
+    @classmethod
+    def setUpTestData(self):
+        """Setup Test
+
+        1. Create an org
+        2. Create an item
+        """
+
+        organization = Organization.objects.create(name='test_org')
+
+        self.organization = organization
+
+
+    def test_serializer_validation_no_name(self):
+        """Serializer Validation Check
+
+        Ensure that if creating and no name is provided a validation error occurs
+        """
+
+        with pytest.raises(ValidationError) as err:
+
+            serializer = DeviceModelModelSerializer(data={
+                "organization": self.organization.id,
+            })
+
+            serializer.is_valid(raise_exception = True)
+
+        assert err.value.get_codes()['name'][0] == 'required'
