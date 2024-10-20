3cd6182cb715ed07b1a6174849bebfa9f6b35401
Jon <jonathon.lockwood@networkedweb.com>
2024-10-20 17:19:08 +0930

    test(itam): Device Type Serializer Validation checks
    ref: #15 #248 #353

---
+++ app/itam/tests/unit/device_type/test_device_type_serializer.py
@@ -0,0 +1,52 @@
+import pytest
+
+from django.test import TestCase
+
+from rest_framework.exceptions import ValidationError
+
+from access.models import Organization
+
+from itam.serializers.device_type import DeviceType, DeviceTypeModelSerializer
+
+
+
+class DeviceTypeValidationAPI(
+    TestCase,
+):
+
+    model = DeviceType
+
+    @classmethod
+    def setUpTestData(self):
+        """Setup Test
+
+        1. Create an org
+        2. Create an item
+        """
+
+        organization = Organization.objects.create(name='test_org')
+
+        self.organization = organization
+
+        self.item = self.model.objects.create(
+            organization=organization,
+            name = 'valid-hostname',
+        )
+
+
+
+    def test_serializer_validation_no_name(self):
+        """Serializer Validation Check
+
+        Ensure that if creating and no name is provided a validation error occurs
+        """
+
+        with pytest.raises(ValidationError) as err:
+
+            serializer = DeviceTypeModelSerializer(data={
+                "organization": self.organization.id,
+            })
+
+            serializer.is_valid(raise_exception = True)
+
+        assert err.value.get_codes()['name'][0] == 'required'
