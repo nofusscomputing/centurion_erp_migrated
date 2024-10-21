2ebc18726f0dfeb7929cd19d4e50424de2f9fb79
Jon <jonathon.lockwood@networkedweb.com>
2024-10-21 15:24:29 +0930

    feat(itim): Add Cluster API v2 endpoint
    ref: #248 #356

--- app/itim/viewsets/index.py
+++ app/itim/viewsets/index.py
@@ -26,7 +26,7 @@ class Index(CommonViewSet):
         return Response(
             {
                 "change": "ToDo",
-                "cluster": "ToDo",
+                "cluster": reverse('API:_api_v2_cluster-list', request=request),
                 "incident": "ToDo",
                 "problem": "ToDo",
                 "service": reverse('API:_api_v2_service-list', request=request),
