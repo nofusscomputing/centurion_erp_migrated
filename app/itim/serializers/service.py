f1c5ebca71a5a5eac539efc1072a6a62bb2b3463
Jon <jonathon.lockwood@networkedweb.com>
2024-10-21 14:37:44 +0930

    feat(itim): Add Service API v2 endpoint
    ref: #248 #356

--- app/itim/serializers/service.py
+++ app/itim/serializers/service.py
@@ -3,6 +3,10 @@ from rest_framework import serializers
 
 from access.serializers.organization import OrganizationBaseSerializer
 
+from itam.serializers.device import DeviceBaseSerializer
+
+from itim.serializers.cluster import ClusterBaseSerializer
+from itim.serializers.port import PortBaseSerializer
 from itim.models.services import Service
 
 
@@ -15,6 +19,10 @@ class ServiceBaseSerializer(serializers.ModelSerializer):
 
         return str( item )
 
+    url = serializers.HyperlinkedIdentityField(
+        view_name="API:_api_v2_service-detail", format="html"
+    )
+
     class Meta:
 
         model = Service
@@ -23,10 +31,87 @@ class ServiceBaseSerializer(serializers.ModelSerializer):
             'id',
             'display_name',
             'name',
+            'url',
         ]
 
         read_only_fields = [
             'id',
             'display_name',
             'name',
+            'url',
         ]
+
+
+class ServiceModelSerializer(ServiceBaseSerializer):
+
+    _urls = serializers.SerializerMethodField('get_url')
+
+    def get_url(self, item):
+
+        return {
+            '_self': reverse("API:_api_v2_service-detail", request=self._context['view'].request, kwargs={'pk': item.pk}),
+            'history': reverse(
+                "API:_api_v2_model_history-list",
+                request=self._context['view'].request,
+                kwargs={
+                    'model_class': self.Meta.model._meta.model_name,
+                    'model_id': item.pk
+                }
+            ),
+            'notes': reverse("API:_api_v2_service_notes-list", request=self._context['view'].request, kwargs={'service_id': item.pk}),
+            'tickets': 'ToDo'
+        }
+
+
+    rendered_config = serializers.JSONField(source='config_variables')
+
+
+    class Meta:
+
+        model = Service
+
+        fields =  [
+             'id',
+            'organization',
+            'display_name',
+            'name',
+            'model_notes',
+            'is_template',
+            'template',
+            'device',
+            'cluster',
+            'config',
+            'rendered_config',
+            'config_key_variable',
+            'port',
+            'dependent_service',
+            'is_global',
+            'created',
+            'modified',
+            '_urls',
+        ]
+
+        read_only_fields = [
+            'id',
+            'display_name',
+            'rendered_config',
+            'created',
+            'modified',
+            '_urls',
+        ]
+
+
+
+class ServiceViewSerializer(ServiceModelSerializer):
+
+    cluster = ClusterBaseSerializer( many = False, read_only = True )
+
+    device = DeviceBaseSerializer( many = False, read_only = True )
+
+    dependent_service = ServiceBaseSerializer( many = True, read_only = True )
+
+    organization = OrganizationBaseSerializer( many = False, read_only = True )
+
+    port = PortBaseSerializer( many = True, read_only = True )
+
+    template = ServiceBaseSerializer( many = False, read_only = True )
