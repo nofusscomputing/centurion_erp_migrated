385f654137d5146b6f3e135a9cddfac194918a98
Jon <jonathon.lockwood@networkedweb.com>
2024-10-22 10:52:33 +0930

    feat(project_management): Add Project API v2 endpoint
    ref: #248 #357

---
+++ app/project_management/serializers/project.py
@@ -0,0 +1,110 @@
+from rest_framework.reverse import reverse
+from rest_framework import serializers
+
+from access.serializers.organization import OrganizationBaseSerializer
+
+from itam.serializers.device import DeviceBaseSerializer
+
+from project_management.models.projects import Project
+
+
+
+class ProjectBaseSerializer(serializers.ModelSerializer):
+
+    display_name = serializers.SerializerMethodField('get_display_name')
+
+    def get_display_name(self, item):
+
+        return str( item )
+
+    url = serializers.HyperlinkedIdentityField(
+        view_name="API:_api_v2_project-detail", format="html"
+    )
+
+    class Meta:
+
+        model = Project
+
+        fields = [
+            'id',
+            'display_name',
+            'name',
+            'url',
+        ]
+
+        read_only_fields = [
+            'id',
+            'display_name',
+            'name',
+            'url',
+        ]
+
+
+class ProjectModelSerializer(ProjectBaseSerializer):
+
+    _urls = serializers.SerializerMethodField('get_url')
+
+    def get_url(self, item):
+
+        return {
+            '_self': reverse("API:_api_v2_project-detail", request=self._context['view'].request, kwargs={'pk': item.pk}),
+            'history': reverse(
+                "API:_api_v2_model_history-list",
+                request=self._context['view'].request,
+                kwargs={
+                    'model_class': self.Meta.model._meta.model_name,
+                    'model_id': item.pk
+                }
+            ),
+            'notes': reverse("API:_api_v2_cluster_notes-list", request=self._context['view'].request, kwargs={'cluster_id': item.pk}),
+            'tickets': 'ToDo'
+        }
+
+
+    class Meta:
+
+        model = Project
+
+        fields = '__all__'
+
+        fields =  [
+            'id',
+            'external_ref',
+            'external_system',
+            'organization',
+            'display_name',
+            'name',
+            'description',
+            'priority',
+            'state',
+            'project_type',
+            'code',
+            'planned_start_date',
+            'planned_finish_date',
+            'real_start_date',
+            'real_finish_date',
+            'manager_user',
+            'manager_team',
+            'team_members',
+            'is_deleted',
+
+
+            'is_global',
+            'created',
+            'modified',
+            '_urls',
+        ]
+
+        read_only_fields = [
+            'id',
+            'display_name',
+            'created',
+            'modified',
+            '_urls',
+        ]
+
+
+
+class ProjectViewSerializer(ProjectModelSerializer):
+
+    organization = OrganizationBaseSerializer( many = False, read_only = True )
