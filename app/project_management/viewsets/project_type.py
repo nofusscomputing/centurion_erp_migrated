ce566a8928df2f2ca0e6fcd60caed2a70ca8fe52
Jon <jonathon.lockwood@networkedweb.com>
2024-10-22 12:02:29 +0930

    feat(project_management): Add Project Type API v2 endpoint
    ref: #248 #357

--- app/project_management/viewsets/project_type.py
+++ app/project_management/viewsets/project_type.py
@@ -2,26 +2,26 @@ from drf_spectacular.utils import extend_schema, extend_schema_view, OpenApiResp
 
 from api.viewsets.common import ModelViewSet
 
-from project_management.serializers.project import (
-    Project,
-    ProjectModelSerializer,
-    ProjectViewSerializer
+from project_management.serializers.project_type import (
+    ProjectType,
+    ProjectTypeModelSerializer,
+    ProjectTypeViewSerializer
 )
 
 
 
 @extend_schema_view(
     create=extend_schema(
-        summary = 'Create a cluster',
+        summary = 'Create a project type',
         description='',
         responses = {
-            201: OpenApiResponse(description='Device created', response=ProjectViewSerializer),
+            201: OpenApiResponse(description='Device created', response=ProjectTypeViewSerializer),
             400: OpenApiResponse(description='Validation failed.'),
             403: OpenApiResponse(description='User is missing create permissions'),
         }
     ),
     destroy = extend_schema(
-        summary = 'Delete a cluster',
+        summary = 'Delete a project type',
         description = '',
         responses = {
             204: OpenApiResponse(description=''),
@@ -29,27 +29,27 @@ from project_management.serializers.project import (
         }
     ),
     list = extend_schema(
-        summary = 'Fetch all clusters',
+        summary = 'Fetch all project types',
         description='',
         responses = {
-            200: OpenApiResponse(description='', response=ProjectViewSerializer),
+            200: OpenApiResponse(description='', response=ProjectTypeViewSerializer),
             403: OpenApiResponse(description='User is missing view permissions'),
         }
     ),
     retrieve = extend_schema(
-        summary = 'Fetch a single cluster',
+        summary = 'Fetch a single project type',
         description='',
         responses = {
-            200: OpenApiResponse(description='', response=ProjectViewSerializer),
+            200: OpenApiResponse(description='', response=ProjectTypeViewSerializer),
             403: OpenApiResponse(description='User is missing view permissions'),
         }
     ),
     update = extend_schema(exclude = True),
     partial_update = extend_schema(
-        summary = 'Update a cluster',
+        summary = 'Update a project type',
         description = '',
         responses = {
-            200: OpenApiResponse(description='', response=ProjectViewSerializer),
+            200: OpenApiResponse(description='', response=ProjectTypeViewSerializer),
             403: OpenApiResponse(description='User is missing change permissions'),
         }
     ),
@@ -58,18 +58,14 @@ class ViewSet( ModelViewSet ):
 
     filterset_fields = [
         'organization',
-        'external_system',
-        'priority',
-        'project_type',
-        'state',
+        'is_global',
     ]
 
     search_fields = [
         'name',
-        'description',
     ]
 
-    model = Project
+    model = ProjectType
 
     documentation: str = 'https://nofusscomputing.com/docs/not_model_docs'
 
@@ -82,7 +78,7 @@ class ViewSet( ModelViewSet ):
             or self.action == 'retrieve'
         ):
 
-            return globals()[str( self.model._meta.verbose_name) + 'ViewSerializer']
+            return globals()[str( self.model._meta.verbose_name).replace(' ', '') + 'ViewSerializer']
 
 
-        return globals()[str( self.model._meta.verbose_name) + 'ModelSerializer']
+        return globals()[str( self.model._meta.verbose_name).replace(' ', '') + 'ModelSerializer']
