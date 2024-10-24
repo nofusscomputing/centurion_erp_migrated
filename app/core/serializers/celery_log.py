95ab2981b63e3120f5cbdf494a0e12448d1e67d9
Jon <jonathon.lockwood@networkedweb.com>
2024-10-24 13:33:58 +0930

    feat(settings): Add Celery Task Logs API v2 endpoint
    ref: #248 #360

---
+++ app/core/serializers/celery_log.py
@@ -0,0 +1,70 @@
+import json
+
+from rest_framework.reverse import reverse
+from rest_framework import serializers
+
+from django_celery_results.models import TaskResult
+
+from access.serializers.organization import OrganizationBaseSerializer
+
+from app.serializers.user import UserBaseSerializer
+
+
+
+class TaskResultBaseSerializer(serializers.ModelSerializer):
+
+    display_name = serializers.SerializerMethodField('get_display_name')
+
+    def get_display_name(self, item):
+
+        return str( item )
+
+    url = serializers.HyperlinkedIdentityField(
+        view_name="API:_api_v2_manufacturer-detail", format="html"
+    )
+
+
+    class Meta:
+
+        model = TaskResult
+
+        fields = [
+            'id',
+            'display_name',
+            'url',
+        ]
+
+        read_only_fields = [
+            'id',
+            'display_name',
+            'url',
+        ]
+
+
+class TaskResultModelSerializer(TaskResultBaseSerializer):
+
+
+    _urls = serializers.SerializerMethodField('get_url')
+
+    def get_url(self, item):
+
+        return {
+            '_self': reverse("API:_api_v2_manufacturer-detail", 
+                request=self._context['view'].request,
+                kwargs={
+                    'pk': item.pk
+                }
+            ),
+        }
+
+
+    class Meta:
+
+        model = TaskResult
+
+        fields = '__all__'
+
+
+class TaskResultViewSerializer(TaskResultModelSerializer):
+
+    pass
