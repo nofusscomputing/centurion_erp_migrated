99aa66a7aff3b57cf782ac7d824e46c0055c7444
Jon <jonathon.lockwood@networkedweb.com>
2024-10-24 12:42:24 +0930

    feat(settings): Add User Settings API v2 endpoint
    ref: #248 #360

---
+++ app/settings/serializers/user_settings.py
@@ -0,0 +1,87 @@
+from rest_framework.reverse import reverse
+
+from rest_framework import serializers
+
+from access.serializers.organization import OrganizationBaseSerializer
+
+from settings.models.user_settings import UserSettings
+
+
+
+class UserSettingsBaseSerializer(serializers.ModelSerializer):
+
+    display_name = serializers.SerializerMethodField('get_display_name')
+
+    def get_display_name(self, item):
+
+        return str( item )
+
+    url = serializers.HyperlinkedIdentityField(
+        view_name="API:_api_v2_user_settings-detail", format="html"
+    )
+
+    class Meta:
+
+        model = UserSettings
+
+        fields = [
+            'id',
+            'display_name',
+            'name',
+            'url',
+        ]
+
+        read_only_fields = [
+            'id',
+            'display_name',
+            'name',
+            'url',
+        ]
+
+
+
+class UserSettingsModelSerializer(UserSettingsBaseSerializer):
+
+    _urls = serializers.SerializerMethodField('get_url')
+
+    def get_url(self, item):
+
+        return {
+            '_self': reverse("API:_api_v2_user_settings-detail", request=self._context['view'].request, kwargs={'pk': item.pk}),
+        }
+
+
+    class Meta:
+
+        model = UserSettings
+
+        fields = '__all__'
+
+        # fields =  [
+        #     'id',
+        #     'organization',
+        #     'display_name',
+        #     'name',
+        #     'template',
+        #     'colour',
+        #     'cluster',
+        #     'devices',
+        #     'software',
+        #     'model_notes',
+        #     'created',
+        #     'modified',
+        #     '_urls',
+        # ]
+
+        # read_only_fields = [
+        #     'id',
+        #     'display_name',
+        #     'created',
+        #     'modified',
+        #     '_urls',
+        # ]
+
+
+class UserSettingsViewSerializer(UserSettingsModelSerializer):
+
+    organization = OrganizationBaseSerializer( many = False, read_only = True )
