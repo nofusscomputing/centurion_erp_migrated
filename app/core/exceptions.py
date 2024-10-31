821ba0edbff53014dc31698bd84acc403a83148a
Jon <jonathon.lockwood@networkedweb.com>
2024-10-31 22:30:00 +0930

    feat(core): Add custom exception class
    ref: #248 #373

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -1,5 +1,20 @@
+from rest_framework import exceptions, status
+from rest_framework.exceptions import (
+    ValidationError,
+    PermissionDenied
+)
 
 class MissingAttribute(Exception):
     """ An attribute is missing"""
 
     pass
+
+class APIError(
+    exceptions.APIException
+):
+
+    status_code = status.HTTP_400_BAD_REQUEST
+
+    default_detail = 'An unknown ERROR occured'
+
+    default_code = 'unknown_error'
