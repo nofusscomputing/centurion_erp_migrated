2d88ddf003c5d0351232a14ab5e05ce08a544ea8
Jon <jonathon.lockwood@networkedweb.com>
2024-11-05 22:12:28 +0930

    feat(core): New signal for cleaning linked ticket items when the item is deleted
    ref: #15 #248 #336 #376 #381

---
+++ app/core/signal/ticket_linked_item_delete.py
@@ -0,0 +1,22 @@
+
+from django import dispatch
+from django.dispatch import receiver
+
+from core.models.ticket.ticket_linked_items import TicketLinkedItem
+
+deleted_model = dispatch.Signal()
+
+
+@receiver(deleted_model)
+def signal_deleted_model(sender, item_id, item_type, **kwargs):
+    """Clean up model TicketLinkedItems
+
+    a model was deleted, remove its link to any tickets it had.
+    """
+
+    items = TicketLinkedItem.objects.filter(
+        item_type = item_type,
+        item = item_id
+    )
+
+    items.delete()
