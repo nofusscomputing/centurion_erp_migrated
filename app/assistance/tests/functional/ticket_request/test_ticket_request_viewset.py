2429a94c7d3bcca6c6a905edb22a1b6df9e7cf16
Jon <jonathon.lockwood@networkedweb.com>
2024-11-26 01:59:49 +0930

    test(assistance): Request ticket Viewset Serializer checks
    ref: #15 #392 #393

--- app/assistance/tests/functional/ticket_request/test_ticket_request_viewset.py
+++ app/assistance/tests/functional/ticket_request/test_ticket_request_viewset.py
@@ -1,13 +1,31 @@
 from django.test import TestCase
 
-from core.tests.abstract.test_ticket_viewset import Ticket, TicketViewSetPermissionsAPI
+from core.tests.abstract.test_ticket_viewset import Ticket, TicketViewSetBase, TicketViewSetPermissionsAPI, TicketViewSet
 
 
-class TicketRequestPermissionsAPI(
-    TicketViewSetPermissionsAPI,
-    TestCase,
-):
+
+class ViewSetBase( TicketViewSetBase ):
 
     ticket_type = 'request'
 
     ticket_type_enum = Ticket.TicketType.REQUEST
+
+
+
+class TicketRequestPermissionsAPI(
+    ViewSetBase,
+    TicketViewSetPermissionsAPI,
+    TestCase,
+):
+
+    pass
+
+
+
+class TicketRequestViewSet(
+    TicketViewSet,
+    ViewSetBase,
+    TestCase,
+):
+
+    pass
