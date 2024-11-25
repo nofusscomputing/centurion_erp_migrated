0d9cab032b8bc555b0a4ab3d972fdffc174519a8
Jon <jonathon.lockwood@networkedweb.com>
2024-11-26 02:11:31 +0930

    test(assistance): Problem ticket Viewset Serializer checks
    ref: #15 #392 #393

--- app/itim/tests/functional/ticket_problem/test_ticket_problem_viewset.py
+++ app/itim/tests/functional/ticket_problem/test_ticket_problem_viewset.py
@@ -1,13 +1,30 @@
 from django.test import TestCase
 
-from core.tests.abstract.test_ticket_viewset import Ticket, TicketViewSetPermissionsAPI
+from core.tests.abstract.test_ticket_viewset import Ticket, TicketViewSetBase, TicketViewSetPermissionsAPI, TicketViewSet
 
 
-class TicketProblemPermissionsAPI(
-    TicketViewSetPermissionsAPI,
-    TestCase,
-):
+class ViewSetBase( TicketViewSetBase ):
 
     ticket_type = 'problem'
 
     ticket_type_enum = Ticket.TicketType.PROBLEM
+
+
+
+class TicketProblemPermissionsAPI(
+    ViewSetBase,
+    TicketViewSetPermissionsAPI,
+    TestCase,
+):
+
+    pass
+
+
+
+class TicketProblemViewSet(
+    TicketViewSet,
+    ViewSetBase,
+    TestCase,
+):
+
+    pass
