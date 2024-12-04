7f46daeb54de76789f91739e0a472d4b92c47ffa
Jon <jonathon.lockwood@networkedweb.com>
2024-12-04 23:20:26 +0930

    test(itim): API Metadata checks for cluster type
    ref: #410 #412 #415

--- app/itim/tests/functional/cluster_types/test_cluster_type_viewset.py
+++ app/itim/tests/functional/cluster_types/test_cluster_type_viewset.py
@@ -8,6 +8,7 @@ from access.models import Organization, Team, TeamUsers, Permission
 
 from api.tests.abstract.api_permissions_viewset import APIPermissions
 from api.tests.abstract.api_serializer_viewset import SerializersTestCases
+from api.tests.abstract.test_metadata_functional import MetadataAttributesFunctional
 
 from itim.models.clusters import ClusterType
 
@@ -190,5 +191,15 @@ class ClusterTypePermissionsAPI(ViewSetBase, APIPermissions, TestCase):
 
 
 class ClusterTypeViewSet(ViewSetBase, SerializersTestCases, TestCase):
+
+    pass
+
+
+
+class ClusterTypeMetadata(
+    ViewSetBase,
+    MetadataAttributesFunctional,
+    TestCase
+):
 
     pass
