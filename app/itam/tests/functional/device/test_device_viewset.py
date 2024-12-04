eb4a132f394eb7988b8047ce564c43fbf48d3d56
Jon <jonathon.lockwood@networkedweb.com>
2024-12-04 23:18:28 +0930

    test(itam): API Metadata checks for device
    ref: #410 #412 #415

--- app/itam/tests/functional/device/test_device_viewset.py
+++ app/itam/tests/functional/device/test_device_viewset.py
@@ -6,9 +6,10 @@ from django.shortcuts import reverse
 from django.test import Client, TestCase
 
 from access.models import Organization, Team, TeamUsers, Permission
-from api.tests.abstract.api_serializer_viewset import SerializersTestCases
 
+from api.tests.abstract.api_serializer_viewset import SerializersTestCases
 from api.tests.abstract.api_permissions_viewset import APIPermissions
+from api.tests.abstract.test_metadata_functional import MetadataAttributesFunctional, MetaDataNavigationEntriesFunctional
 
 from itam.models.device import Device
 
@@ -199,3 +200,16 @@ class DeviceViewSet(
 ):
 
     pass
+
+
+
+class DeviceMetadata(
+    ViewSetBase,
+    MetadataAttributesFunctional,
+    MetaDataNavigationEntriesFunctional,
+    TestCase
+):
+
+    menu_id = 'itam'
+
+    menu_entry_id = 'device'
