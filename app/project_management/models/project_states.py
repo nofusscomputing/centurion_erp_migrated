4bddbd151d1adf5a4a05607f920b85cfa1c630f5
Jon <jonathon.lockwood@networkedweb.com>
2024-12-21 17:10:09 +0930

    feat(information_management): Ability to link Knowledge Base article to a Project State
    ref: #426 #427

--- app/project_management/models/project_states.py
+++ app/project_management/models/project_states.py
@@ -90,6 +90,16 @@ class ProjectState(ProjectStateCommonFields):
             ]
         },
         {
+            "name": "Knowledge Base",
+            "slug": "kb_articles",
+            "sections": [
+                {
+                    "layout": "table",
+                    "field": "knowledge_base",
+                }
+            ]
+        },
+        {
             "name": "Notes",
             "slug": "notes",
             "sections": []
