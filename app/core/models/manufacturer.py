803491f04867ccaf48cc71fbb0181ff47b148634
Jon <jonathon.lockwood@networkedweb.com>
2024-12-21 17:06:49 +0930

    feat(information_management): Ability to link Knowledge Base article to a Manufacturer
    ref: #426 #427

--- app/core/models/manufacturer.py
+++ app/core/models/manufacturer.py
@@ -73,6 +73,16 @@ class Manufacturer(TenancyObject, ManufacturerCommonFields, SaveHistory):
             ]
         },
         {
+            "name": "Knowledge Base",
+            "slug": "kb_articles",
+            "sections": [
+                {
+                    "layout": "table",
+                    "field": "knowledge_base",
+                }
+            ]
+        },
+        {
             "name": "Notes",
             "slug": "notes",
             "sections": []
