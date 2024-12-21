81dbc30027aa0d0c1e15c711a428e10bea729f78
Jon <jonathon.lockwood@networkedweb.com>
2024-12-21 17:13:00 +0930

    feat(information_management): Ability to link Knowledge Base article to a Device Model
    ref: #331 #426 #427 #428

--- app/itam/serializers/device_model.py
+++ app/itam/serializers/device_model.py
@@ -55,7 +55,15 @@ class DeviceModelModelSerializer(
     def get_url(self, obj) -> dict:
 
         return {
-            '_self': obj.get_url( request = self._context['view'].request )
+            '_self': obj.get_url( request = self._context['view'].request ),
+            'knowledge_base': reverse(
+                "v2:_api_v2_model_kb-list",
+                request=self._context['view'].request,
+                kwargs={
+                    'model': self.Meta.model._meta.model_name,
+                    'model_pk': item.pk
+                }
+            ),
         }
 
     class Meta:
